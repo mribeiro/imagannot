42be19550742dbdbe10f60674b96ff80abce497e
pmilheiro <pedro.m.milheiro@wit-software.com>
2014-03-26 01:43:51 +0000

    + Added delete all annotations button + Added annotation sizes * Added image scrolling instead of image rescaling *  Minor improvements

--- src/org/aribeiro/imagannot/shapes/RoundLabel.java
+++ src/org/aribeiro/imagannot/shapes/RoundLabel.java
@@ -20,8 +20,9 @@ public class RoundLabel extends CommonLabel {
 
     private final Color backgroundColor;
     
-    public RoundLabel(String text, Color bgColor, Color textColor) {
-        super(text, bgColor, textColor, null, new Dimension(21, 21));
+    public RoundLabel(String text, Color bgColor, Color textColor, Dimension dim) {
+//        super(text, bgColor, textColor, null, new Dimension(21, 21));
+        super(text, bgColor, textColor, null, dim);
         this.backgroundColor = bgColor;
         setOpaque(false);
     }
@@ -34,8 +35,19 @@ public class RoundLabel extends CommonLabel {
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         
-        g2d.draw(new Ellipse2D.Float(0, 0, 20, 20));
-        g2d.fill(new Ellipse2D.Float(1, 1, 19, 19));
+//        g2d.draw(new Ellipse2D.Float(0, 0, 20, 20));
+//        g2d.fill(new Ellipse2D.Float(1, 1, 19, 19));
+        
+        float width = Float.parseFloat(this.getSize().getWidth()+"");
+        float height = Float.parseFloat(this.getSize().getHeight()+"");
+        
+        width-=1;
+        height-=1;
+        g2d.draw(new Ellipse2D.Double(0, 0, width, height));
+        
+        width-=1;
+        height-=1;
+        g2d.fill(new Ellipse2D.Double(1, 1, width, height));
         
         super.paintComponent(g);
     }
--- src/org/aribeiro/imagannot/utils/ImageExporter.java
+++ src/org/aribeiro/imagannot/utils/ImageExporter.java
@@ -3,46 +3,63 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.aribeiro.imagannot.utils;
 
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import javax.imageio.ImageIO;
-import javax.swing.JPanel;
-import javax.swing.border.Border;
+import javax.swing.CellRendererPane;
 
 /**
  *
  * @author Miguel
  */
 public final class ImageExporter {
-    
-    private ImageExporter() {
-        
-    }
-    
-    public static BufferedImage panelToImage(JPanel panel) {
-        Border b = panel.getBorder();
-        panel.setBorder(null);
-        BufferedImage img = new BufferedImage(panel.getWidth(), panel.getHeight(), BufferedImage.TYPE_INT_RGB);
-        Graphics2D graphics = (Graphics2D) img.getGraphics();
-        graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        panel.paintComponents(graphics);
-        panel.setBorder(b);
-        return img;
-    }
-    
-    public static void savePanelAsImage(JPanel panel, String path) {
-        try {
-            ImageIO.write(ImageExporter.panelToImage(panel), "png", new File(path));
-            System.out.println("panel saved as image");
 
-        } catch (Exception e) {
+    private ImageExporter() {
+
+    }
+
+    public static void savePanelAsImage(Component component, String path) {
+        try {
+            ImageIO.write(ImageExporter.componentToImage(component, true), "png", new File(path));
+            System.out.println("panel saved as image");
+        } catch (IOException e) {
             System.out.println("panel not saved" + e.getMessage());
         }
     }
-    
+
+    public static BufferedImage componentToImage(Component component, boolean visible) {
+        if (visible) {
+            BufferedImage img = new BufferedImage(component.getWidth(), component.getHeight(), BufferedImage.TRANSLUCENT);
+            Graphics2D g2d = (Graphics2D) img.getGraphics();
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            component.paintAll(g2d);
+            return img;
+        } else {
+            component.setSize(component.getPreferredSize());
+            layoutComponent(component);
+            BufferedImage img = new BufferedImage(component.getWidth(), component.getHeight(), BufferedImage.TRANSLUCENT);
+            CellRendererPane crp = new CellRendererPane();
+            crp.add(component);
+            crp.paintComponent(img.createGraphics(), component, crp, component.getBounds());
+            return img;
+        }
+    }
+
+    private static void layoutComponent(Component c) {
+        synchronized (c.getTreeLock()) {
+            c.doLayout();
+            if (c instanceof Container) {
+                for (Component child : ((Container) c).getComponents()) {
+                    layoutComponent(child);
+                }
+            }
+        }
+    }
 }
